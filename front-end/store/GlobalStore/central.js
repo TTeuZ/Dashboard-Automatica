f47c81cda22a7be5d7eae590152ed7c2367d6de1
TTeuZ <paulomateus.luza@hotmail.com>
2021-07-10 12:07:56 -0300

    [add] reestruturação do Vuex para separar a entity(firebase), configuração de paginas e globais

---
+++ front-end/store/GlobalStore/central.js
@@ -0,0 +1,44 @@
+import schema from './schema'
+
+const state = {
+  tableData: {},
+  isDataLoading: false,
+  pageSchemaDefined: {
+    title: 'Home',
+  },
+}
+
+const getters = {
+  tableData: (state) => state.tableData,
+  isLoading: (state) => state.isDataLoading,
+  pageSchema: (state) => state.pageSchemaDefined,
+}
+
+const mutations = {
+  SET_TABLE_DATA: (state, payload) => {
+    state.tableData = payload
+  },
+  SET_LOADING: (state, payload) => {
+    state.isDataLoading = payload
+  },
+  SET_PAGE_SCHEMA: (state, payload) => {
+    state.pageSchemaDefined = schema.schemas[payload]
+  },
+}
+
+const actions = {
+  setTableData: async ({ commit, state }) => {
+    // commit('SET_LOADING', true)
+    // await this.$axios.get(state.entity).then((res) => {
+    //   commit('SET_TABLE_DATA', res.data)
+    //   commit('SET_LOADING', false)
+    // })
+  },
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+}
