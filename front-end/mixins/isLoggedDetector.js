9279ca1b81dfc592366b057d8013c531fe681928
TTeuZ <paulomateus.luza@hotmail.com>
2021-07-11 15:14:19 -0300

    [add] watcher para verificar a mudança de estado de login

--- front-end/mixins/isLoggedDetector.js
+++ front-end/mixins/isLoggedDetector.js
@@ -3,13 +3,26 @@ export const isLogged = {
   computed: {
     ...mapGetters(['isAuthenticated']),
   },
-  beforeCreate() {
+  methods: {
+    sendTo(path) {
+      this.$router.push(path)
+    },
+  },
+  created() {
     // Middleware de verificação de esta logado
-
-    if (this.isAuthenticated) {
-      this.$router.push('/login')
+    if (!this.isAuthenticated) {
+      this.sendTo('/login')
     } else {
-      this.$router.push('/')
+      this.sendTo('/')
     }
+  },
+  watch: {
+    isAuthenticated: {
+      handler(newValue) {
+        if (newValue === false) {
+          this.sendTo('/login')
+        }
+      },
+    },
   },
 }
