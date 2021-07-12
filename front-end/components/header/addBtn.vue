f120c9b7b5bc84a9dd3f9a2782d10369bf69128a
TTeuZ <paulomateus.luza@hotmail.com>
2021-07-11 23:52:31 -0300

    [add] estruturas visuais dos componentes

---
+++ front-end/components/header/addBtn.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn color="success" text rounded depressed outlined>
+    <v-icon> mdi-bookmark-plus-outline </v-icon> Adicionar
+  </v-btn>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ front-end/components/header/search.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-text-field
+    v-model="pageSchema.table.search"
+    label="O que procura?"
+    color="#FF6347"
+    dense
+    rounded
+    hide-details
+    outlined
+    clearable
+    append-icon="mdi-folder-search-outline"
+  />
+  <!-- trocar o v-model por uma mutation do vuex -->
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['pageSchema']),
+  },
+}
+</script>
+
+<style scoped></style>
