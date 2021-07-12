564459794dc56ccd939a57b983cb84049366e07c
TTeuZ <paulomateus.luza@hotmail.com>
2021-07-11 23:52:52 -0300

    [add] vinculo com o vuex

--- front-end/components/Table.vue
+++ front-end/components/Table.vue
@@ -1,19 +1,32 @@
 <template>
   <v-container>
-    <v-data-table
-      :headers="tableHeaders"
-      :items="values"
-      :search="search"
-    ></v-data-table>
+    <v-data-table calculate-widths :search="search"></v-data-table>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
       search: '',
     }
+  },
+  computed: {
+    ...mapGetters(['pageSchema']),
+    pageSearch() {
+      return 'ok' // verificar pq aqui ele esta dando undefined para pageSchema.table.search
+    },
+  },
+  watch: {
+    pageSearch: {
+      handler(newValue) {
+        this.search = newValue
+      },
+    },
+  },
+  mounted() {
+    this.search = this.pageSearch
   },
 }
 </script>
