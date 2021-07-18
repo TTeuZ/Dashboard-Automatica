8e2f3b9c309e63ed231d0a4159eecf9c2df01db6
TTeuZ <paulomateus.luza@hotmail.com>
2021-07-18 14:02:39 -0300

    [add] field de input

--- front-end/components/forms/fields/input.vue
+++ front-end/components/forms/fields/input.vue
@@ -1,10 +1,31 @@
 <template>
-  <!-- <v-text-field /> -->
-  <div>input padrao</div>
+  <v-text-field
+    v-model="inputData"
+    v-maska="component.mask"
+    :label="component.label"
+    rounded
+    outlined
+    dense
+    flat
+    solo
+    validate-on-blur
+    :rules="component.rules"
+    @blur="sendValue(component.key)"
+  />
 </template>
 
 <script>
-export default {}
+import { form } from '~/mixins/forms'
+export default {
+  mixins: [form],
+  props: {
+    component: {
+      type: Object,
+      Required: true,
+      default: () => {},
+    },
+  },
+}
 </script>
 
 <style scoped></style>
