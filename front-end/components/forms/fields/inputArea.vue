b8fb507e65bf56d7bf86f5aba98c2e604a0cf362
TTeuZ <paulomateus.luza@hotmail.com>
2021-07-19 22:20:51 -0300

    [add] field de input area

--- front-end/components/forms/fields/inputArea.vue
+++ front-end/components/forms/fields/inputArea.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>input area</div>
+  <v-textarea
+    v-model="inputData"
+    :label="component.label"
+    rounded
+    outlined
+    flat
+    solo
+    :rules="component.rules"
+    @blur="sendValue(component.key)"
+  />
 </template>
 
 <script>
-export default {}
+import { form } from '~/mixins/forms'
+export default {
+  mixins: [form],
+  props: {
+    component: {
+      type: Object,
+      Required: true,
+      default: () => {},
+    },
+  },
+}
 </script>
 
 <style scoped></style>
