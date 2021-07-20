40e2bed1f6b4b2b36d645ceaebf12430a043a417
TTeuZ <paulomateus.luza@hotmail.com>
2021-07-19 22:21:02 -0300

    [add] fied de select

--- front-end/components/forms/fields/select.vue
+++ front-end/components/forms/fields/select.vue
@@ -1,9 +1,40 @@
 <template>
-  <div></div>
+  <v-select
+    v-model="inputData"
+    :label="component.label"
+    :rules="component.rules"
+    :multiple="component.isMultiple"
+    :items="component.items"
+    :item-value="component.items.value"
+    item-text="label"
+    dense
+    rounded
+    clearable
+    small-chips
+    outlined
+    flat
+    solo
+    @blur="sendValue(component.key)"
+  />
 </template>
 
 <script>
-export default {}
+import { form } from '~/mixins/forms'
+export default {
+  mixins: [form],
+  props: {
+    component: {
+      type: Object,
+      Required: true,
+      default: () => {},
+    },
+  },
+  methods: {
+    getDataFromOutside() {
+      // fazer função que vai pegar data de rotas do firebase.
+    },
+  },
+}
 </script>
 
 <style scoped></style>
