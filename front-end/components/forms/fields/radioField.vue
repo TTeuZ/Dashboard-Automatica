a6554948ed5e223789d1e9fcf11d477015bf464e
TTeuZ <paulomateus.luza@hotmail.com>
2021-08-14 15:17:03 -0300

    [add] radio field

---
+++ front-end/components/forms/fields/radioField.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-radio-group
+    v-model="inputData"
+    class="pt-0 mt-0"
+    row
+    :rules="component.rules"
+    @change="sendValue(component.key)"
+  >
+    <v-radio
+      v-for="(item, i) in component.items"
+      :key="i"
+      :color="component.color"
+      :label="item.label"
+      :value="item.value"
+    ></v-radio>
+  </v-radio-group>
+</template>
+
+<script>
+import { form } from '~/mixins/forms'
+export default {
+  mixins: [form],
+  props: {
+    component: {
+      type: Object,
+      Required: true,
+      default: () => {},
+    },
+  },
+}
+</script>
