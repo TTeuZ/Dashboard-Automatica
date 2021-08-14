67246e0222f314c277d1db17071af6ace6971954
TTeuZ <paulomateus.luza@hotmail.com>
2021-08-14 17:13:16 -0300

    [add] swtichSlot

---
+++ front-end/components/forms/slots/SwitchSlot.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>{{ slotData ? 'Sim' : 'Não' }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    slotData: {
+      type: [Number, String, Array, Object],
+      required: true,
+    },
+    slotConfig: {
+      type: Object,
+      Required: true,
+      default: () => {},
+    },
+  },
+}
+</script>
--- front-end/components/forms/slots/index.js
+++ front-end/components/forms/slots/index.js
@@ -1,9 +1,11 @@
 import fileSlot from './FileSlot.vue'
 import arraySlot from './ArraySlot.vue'
 import onlySlot from './OnlySlot.vue'
+import switchSlot from './SwitchSlot.vue'
 
 export default {
   fileSlot,
   arraySlot,
   onlySlot,
+  switchSlot,
 }
