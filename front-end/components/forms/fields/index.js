a9c9599a0279283f722b95c47346b8720035c94b
TTeuZ <paulomateus.luza@hotmail.com>
2021-08-14 15:16:57 -0300

    [add] switch field

--- front-end/components/forms/fields/index.js
+++ front-end/components/forms/fields/index.js
@@ -1,11 +1,19 @@
-import input from './input.vue'
-import inputArea from './inputArea.vue'
-import select from './select.vue'
-import upload from './upload.vue'
+import input from './inputField.vue'
+import inputArea from './inputAreaField.vue'
+import select from './selectField.vue'
+import upload from './uploadField.vue'
+import number from './numberField.vue'
+import switchField from './switchField.vue'
+import radio from './radioField.vue'
+import checkbox from './checkboxField.vue'
 
 export default {
   textInput: input,
   inputArea,
   vSelect: select,
   upload,
+  numberInput: number,
+  switchField,
+  radio,
+  checkbox,
 }
---
+++ front-end/components/forms/fields/switchField.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-switch
+    v-model="inputData"
+    :label="component.label"
+    rounded
+    class="pt-0 mt-0"
+    @change="sendValue(component.key)"
+  ></v-switch>
+</template>
+
+<script>
+import { form } from '~/mixins/forms'
+export default {
+  mixins: [form],
+  props: {
+    component: {
+      type: Object,
+      Required: true,
+      default: () => {},
+    },
+  },
+}
+</script>
