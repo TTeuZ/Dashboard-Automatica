74e624e1087be6aa634cf10848716aeed6856508
TTeuZ <paulomateus.luza@hotmail.com>
2021-08-27 14:55:32 -0300

    [change] nome do projeto

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -17,7 +17,7 @@ export default {
    */
   head: {
     titleTemplate: '',
-    title: 'Dashboard Automatica',
+    title: 'Kontroll',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
